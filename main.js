169f3fe76e6efddb8af310647abe4bd59fad05ed
Lin Zhang <linroid@gmail.com>
2019-12-18 00:34:44 +0800

    Update logs

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-if (typeof $dora == undefined) {
-  console.log('This project can only run on Dora app, please visit https://dorajs.com for more information')
+if (typeof $dora == 'undefined') {
+  console.error('This project runs only in Dora.js.')
+  console.error('Please visit https://dorajs.com/ for more information.')
   process.exit(-1)
 }
 
-console.log('Congratulation, your addon run successfully!')
+console.info('Congratulation, your addon runs successfully!')
